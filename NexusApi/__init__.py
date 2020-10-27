bda6f15bad967dc0be8f5d2d7217b0909690bec2
doorknob6 <joopkjongste@gmail.com>
2020-10-27 14:35:15 +0100

    nexusapi

--- NexusApi/__init__.py
+++ NexusApi/__init__.py
@@ -1,2 +1,2 @@
-from Api.NexusHub import NexusApi, Price
-from Api.TimeoutHttpAdapter import TimeoutHttpAdapter
+from NexusApi.NexusHub import NexusApi, Price
+from NexusApi.TimeoutHttpAdapter import TimeoutHttpAdapter
